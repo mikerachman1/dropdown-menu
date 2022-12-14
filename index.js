9cdca0ee918e2f9aaf832da6dc93665aaad9016d
Mike Rachman <mikerachman1@gmail.com>
2022-12-14 10:01:22 -0500

    refactor of index.js

--- index.js
+++ index.js
@@ -4,9 +4,5 @@ const dropdownContent = document.querySelector('.dropdown-content');
 dropdownContent.style.display = 'none';
 
 dropdownButton.addEventListener('click', () => {
-  if (dropdownContent.style.display === 'none') {
-    dropdownContent.style.display = 'flex';
-  } else {
-    dropdownContent.style.display = 'none';
-  }
+  dropdownContent.style.display === 'none' ? dropdownContent.style.display = 'flex' : dropdownContent.style.display = 'none';
 })
